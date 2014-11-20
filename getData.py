2b24d218436acf92e5d85942369ce144a2bc899b
kirill Mint <pomogajko@gmail.com>
2014-11-20 23:29:46 +0100

    addFromFeed added

--- getData.py
+++ getData.py
@@ -2,7 +2,7 @@ import os
 from facepy import GraphAPI
 import sqlite3
 import time
-
+import re
 
 token = os.environ["uncoolstars"]
 graph = GraphAPI(token)
@@ -15,10 +15,10 @@ def getFromFB(star):
    # except OAuthError as oauth:
     #    return star * "not a public profile"
     except:
-        return "Could not get account " + star
+        return "Could not get account " + star + "."
     
     if(not data["is_verified"]):
-        return "Not verified"
+        return "Account " + star + "is not verified."
     else:
         return data
 
@@ -44,6 +44,7 @@ def isStarinDB(star):
     star = (star,)
     c.execute("Select count(*) FROM stars Where username = ?", star)
     result = c.fetchone()
+    c.close()
     return result[0] == 1 
 
 def addStar(star):
@@ -65,6 +66,50 @@ def addStar(star):
 
     return True
 
-def addNewStar(star):
-    addEntry(star)
-    
+
+def oldpost(id):
+    c = conn.cursor()
+    id = (id,)
+    c.execute("Select count(*) FROM requests WHERE id = ?", id)
+    result = c.fetchone()
+    c.close()
+    return result[0] == 1
+
+
+
+def addFromFeed():
+    feed = graph.get("me/feed?limit=100")
+    for post in feed["data"]:
+        if ("message" in post.keys()):
+            if len(re.findall("^add ", post["message"])) == 1:
+                if (not oldpost(post["id"])):
+                    star = re.split(" ", post["message"])[1]
+                    fromfb = getFromFB(star)
+                    if(type(fromfb) is str) :
+                        graph.post(path=post["id"]+"/comments", message=fromfb)
+                        print "Request not valid " + post["message"]
+                    else:
+                        newstar = not isStarinDB(star)
+                        ins = [post["id"], post["message"], post["from"]["id"], post["from"]["name"], post["created_time"], time.strftime("%c"), newstar]
+
+                        c = conn.cursor()
+                        c.execute("INSERT into requests values(?,?,?,?,?,?,?)",ins)
+                        c.close()
+                    
+                        if (newstar):
+                            addStar(star)
+                            mess = star + " added to DB and started watching"
+                            graph.post(path=post["id"]+"/comments",message= mess)
+                            print mess
+                        else:
+                            conn.commit()
+                            c = conn.cursor()
+                            c.execute("Select inserted from stars where username = ?", (star,))
+                            inDBsince = c.fetchone()[0]
+                            c.close()
+                            mess = star + " is already in db since " + inDBsince
+                            print mess
+                            graph.post(path=post["id"]+"/comments",message = mess)
+    return True
+
+
